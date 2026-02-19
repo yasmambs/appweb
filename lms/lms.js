a568714202a26a6fd9f768446ba2860227241df1
yasmambs <49554800+yasmambs@users.noreply.github.com>
2026-02-19 20:52:22 +0700

    Update lms.js

--- lms/lms.js
+++ lms/lms.js
@@ -1,10 +1,3 @@
-function tambahMateri(){
-  let materi = JSON.parse(localStorage.getItem("materi")) || [];
-
-  materi.push({
-    judul: document.getElementById("judul").value,
-    isi: document.getElementById("isi").value
-  });
-
-  localStorage.setItem("materi", JSON.stringify(materi));
+function materi(){
+  alert("Materi tampil di sini");
 }
